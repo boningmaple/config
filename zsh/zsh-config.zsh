f8c2bc90411563ca8e79087bbe4b2a81ed284423
boning <boning.maple@gmail.com>
2025-11-29 15:44:14 +1100

    [zsh] config fzf plugin

--- zsh/zsh-config.zsh
+++ zsh/zsh-config.zsh
@@ -1,17 +1,14 @@
-# Alias
-# Make the default `ls` output colorful
-alias ls='ls --color=auto'
-# Same as `ls`, but with file type indicators
-alias l='ls -CF --color=auto'
-# List all files
-alias la='ls -A --color=auto'
-# List all files with details
-alias ll='ls -alF --color=auto'
-
 # Vi mode
 bindkey -v
 
-# History
+# Load completion and styling
+autoload -Uz compinit && compinit
+zstyle ':completion:*' menu select
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} ma=0\;33 # colorize cmp menu
+
+# Opts
+HISTSIZE=1000000
+SAVEHIST=1000000
 HISTDUP=erase
 setopt appendhistory
 setopt sharehistory
@@ -20,9 +17,18 @@ setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_ignore_dups
 setopt hist_find_no_dups
+setopt no_case_glob no_case_match
+setopt interactive_comments
 
-# Load completions
-autoload -Uz compinit && compinit
+# Alias
+# Make the default `ls` output colorful
+alias ls='ls --color'
+# Same as `ls`, but with file type indicators
+alias l='ls -CF --color'
+# List all files
+alias la='ls -A --color'
+# List all files with details
+alias ll='ls -alF --color'
 
 # Activate Zsh plugins
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -30,3 +36,9 @@ source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source <(fzf --zsh)
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
+
+# Fzf config
+export FZF_DEFAULT_OPTS='--no-height --no-reverse'
+export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
